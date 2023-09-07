fcd700195009a6079c5d5236bee78bfb6917aa0d
Rishabh Maurya <75687649+RishabhGithub7348@users.noreply.github.com>
2023-09-07 22:29:52 +0530

    Update index.js

--- src/constant/index.js
+++ src/constant/index.js
@@ -2,33 +2,33 @@ export const Language = [
     {
       language: "Spanish",
       image: "/images/spanish.png",
-      followers: "23.2 learners",
+      followers: "23.2M learners",
     },
    {
         language: "French",
         image: "/images/french.jpg",
-        followers: "55.9 learners",
+        followers: "55.9M learners",
    },
    {
     language: "Russia",
     image: "/images/russia.jpg",
-    followers: "88.6 learners",
+    followers: "88.6M learners",
    },
    {
     language: "German",
     image: "/images/german.jpg",
-    followers: "76.9 learners",
+    followers: "76.9M learners",
    },
    {
     language: "Italian",
     image: "/images/italy.jpg",
-    followers: "66.6 learners",
+    followers: "66.6M learners",
    
    },
    {
     language: "Dutch",
     image: "/images/dutch.jpg",
-    followers: "88.5 learners",
+    followers: "88.5M learners",
    
    }
   ];
